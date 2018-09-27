b7f57ce0ae69ee03d2b13f19b07c93f237dea57e
SDrake01 <35353948+SDrake01@users.noreply.github.com>
2018-09-27 15:22:39 -0400

    Still testing repo connection

--- FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
+++ FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
@@ -3,6 +3,7 @@ package com.example.libjavajoke;
 public class JavaJoke {
 
     // If I add this and commit, which folder does it go into?
+    // does deleting the duplicate break this link?
     public String getJoke(){
         return "Mooooooooo!\n            -Impatient Cow";
     }
