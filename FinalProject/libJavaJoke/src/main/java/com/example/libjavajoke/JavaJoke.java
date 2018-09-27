e4d0d506a8a2afe924f8e76223f12215c52af153
SDrake01 <35353948+SDrake01@users.noreply.github.com>
2018-09-27 15:24:08 -0400

    Synced GitHub Desktop to the correct folder

--- FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
+++ FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
@@ -4,6 +4,7 @@ public class JavaJoke {
 
     // If I add this and commit, which folder does it go into?
     // does deleting the duplicate break this link?
+    // Maybe the final test?
     public String getJoke(){
         return "Mooooooooo!\n            -Impatient Cow";
     }
