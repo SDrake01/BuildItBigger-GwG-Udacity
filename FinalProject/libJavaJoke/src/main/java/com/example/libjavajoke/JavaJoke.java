1acb52122e6b61ddf1e389a4a33f518d32a34b1a
SDrake01 <35353948+SDrake01@users.noreply.github.com>
2018-09-27 15:15:11 -0400

    Testing repo connection and storage location

--- FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
+++ FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
@@ -2,6 +2,7 @@ package com.example.libjavajoke;
 
 public class JavaJoke {
 
+    // If I add this and commit, which folder does it go into?
     public String getJoke(){
         return "Mooooooooo!\n            -Impatient Cow";
     }
