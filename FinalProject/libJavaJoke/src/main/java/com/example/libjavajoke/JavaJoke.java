26484065f235ac8bb6d12f5907dc7761b1b4407d
SDrake01 <35353948+SDrake01@users.noreply.github.com>
2018-09-27 15:26:41 -0400

    Finished setting up GitHub Desktop to show Android Studio commits properly

--- FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
+++ FinalProject/libJavaJoke/src/main/java/com/example/libjavajoke/JavaJoke.java
@@ -2,9 +2,6 @@ package com.example.libjavajoke;
 
 public class JavaJoke {
 
-    // If I add this and commit, which folder does it go into?
-    // does deleting the duplicate break this link?
-    // Maybe the final test?
     public String getJoke(){
         return "Mooooooooo!\n            -Impatient Cow";
     }
